bfff937a962d8ec79de114919caceafea2e5edf3
POFIGISTER <mr.nikiti81@gmail.com>
2023-03-10 12:43:22 +0200

    change db connection

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: 'mongodb+srv://<username>:<password>@cluster0.duym5jc.mongodb.net/?retryWrites=true&w=majority',//'mongodb://localhost:27017/website',
+    db: 'mongodb+srv://admin:admin@cluster0.duym5jc.mongodb.net/?retryWrites=true&w=majority',//'mongodb://localhost:27017/website',
     secret: 'carfixfinder'
 }
