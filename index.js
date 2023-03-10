0533672d598131d76185331a7b4803b0d8c1b4a0
POFIGISTER <mr.nikiti81@gmail.com>
2023-03-10 13:46:12 +0200

    make it better

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const session = require('express-session');
 
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(session({secret: config.secret}));
 app.use(passport.initialize());
